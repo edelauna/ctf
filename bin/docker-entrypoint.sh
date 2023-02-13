879f4535ccb485b97fd5e7772a7890d6fbd6edc2
Elliott de Launay <edelaunay@wealthsimple.com>
2023-02-13 11:37:34 -0500

    feat(docker-entrypoint): testing openvpn setup options

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -2,10 +2,14 @@
 
 #   openvpn setup   #
 #####################
+
+sudo mkdir -p /openvpn{/pid,/sock,/tmp} /dev/net /config/log /config/etc/tmp
+
 if [ ! -c /dev/net/tun ]; then
     sudo mknod /dev/net/tun c  10 200
 fi
 
+# dbus setup
 sudo mkdir -p /var/run/dbus/
 sudo dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
 
